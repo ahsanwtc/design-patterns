7f70c79c1598a617186ced2cb9188f406b000d43
jsan <ahsan.wtc@gmail.com>
2023-01-03 12:52:43 +0100

    implementing composite command

--- src/command/CommandDemo.java
+++ src/command/CommandDemo.java
@@ -5,9 +5,19 @@ import command.framework.Button;
 public class CommandDemo {
   public static boolean exercise = false;
   public static void main() {
+    /* simple command */
     var service = new CustomerService();
     var command = new AddCustomerCommand(service);
     var button = new Button(command);
     button.onClick();
+
+    /* composite command */
+    var commands = new CompositeCommand();
+    commands.add(new ResizeCommand());
+    commands.add(new BlackAndWhiteCommand());
+    commands.execute();
+
+    // replaying a composite command
+    commands.execute();
   }
 }
